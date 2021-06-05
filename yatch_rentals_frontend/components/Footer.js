c2f20f7b147b3f407d38b0ea3f1b0167f6b1f735
Christian Sheen <csheen1725@gmail.com>
2021-06-05 13:07:45 -0400

    Creation of footer component that has copyright and link to about page

---
+++ yatch_rentals_frontend/components/Footer.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import styles from "../styles/Footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>Copyright &copy; Yatch Rentals</p>
+      <p>
+        <Link href="/about">What we are about</Link>
+      </p>
+    </footer>
+  );
+}
