09f6cdfec689a83b4c6f25c6ce535309865cc395
Christian Sheen <csheen1725@gmail.com>
2021-06-05 12:15:37 -0400

    Overlooked a final adjusment with initial styling

--- yatch_rentals_frontend/pages/boats/[slug].js
+++ yatch_rentals_frontend/pages/boats/[slug].js
@@ -1,7 +1,9 @@
+import Layout from "../../components/Layout";
+
 export default function Boat() {
   return (
-    <div>
+    <Layout>
       <h1>This a "Dynamic" individual Boat display</h1>
-    </div>
+    </Layout>
   );
 }
