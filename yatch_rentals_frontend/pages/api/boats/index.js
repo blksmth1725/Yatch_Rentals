c212d4b0d8911abd06c99ce5623626c26583aa43
Christian Sheen <csheen1725@gmail.com>
2021-06-10 14:20:16 -0400

    Limiting methods after noticing that making an api request always performed a get request irrespective of which method was used. Used conditional lofic to determine whether the incoming request was of a particular meth (namely, GET) if true, ir returns boats else it throws an 405 error of no allowed

--- yatch_rentals_frontend/pages/api/boats/index.js
+++ yatch_rentals_frontend/pages/api/boats/index.js
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const { boats } = require("./data.json");
 
 export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+  if (req.method === "GET") {
+    res.status(200).json(boats);
+  } else {
+    res.setHeader("Allow", [GET]);
+    res.status(405).json({ message: `Method ${req.method} is not allowed` });
+  }
+};
--- yatch_rentals_frontend/pages/index.js
+++ yatch_rentals_frontend/pages/index.js
@@ -1,6 +1,6 @@
 import Layout from "@/components/Layout";
 
-export default function HomePage({ boats }) {
+export default function HomePage() {
   return (
     <Layout>
       <h1>Home Page</h1>
