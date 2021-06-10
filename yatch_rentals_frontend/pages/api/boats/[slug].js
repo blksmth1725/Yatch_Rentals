ee985a11a814080ed84a1b9b029da5da284f50e9
Christian Sheen <csheen1725@gmail.com>
2021-06-10 14:30:44 -0400

    Added dynamic slug data retrieval

---
+++ yatch_rentals_frontend/pages/api/boats/[slug].js
@@ -0,0 +1,12 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const { boats } = require("./data.json");
+
+export default (req, res) => {
+  const boat = boats.filter((boat) => boat.slug === req.query.slug);
+  if (req.method === "GET") {
+    res.status(200).json(boat);
+  } else {
+    res.setHeader("Allow", [GET]);
+    res.status(405).json({ message: `Method ${req.method} is not allowed` });
+  }
+};
