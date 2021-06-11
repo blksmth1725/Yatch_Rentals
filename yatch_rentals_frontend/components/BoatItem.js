ab2f5d6342e250d1d3aab8596b047bc5bb2fdb87
Christian Sheen <csheen1725@gmail.com>
2021-06-11 12:51:50 -0400

    Mesing around with the styling of the cards

--- yatch_rentals_frontend/components/BoatItem.js
+++ yatch_rentals_frontend/components/BoatItem.js
@@ -9,6 +9,15 @@ export default function BoatItem({ boat }) {
       <div className={styles.img}>
         <Image src={boat.image} width={170} height={100} />
       </div>
+
+      <div className={styles.info}>
+        <h3>{boat.name}</h3>
+        <div className={styles.span}>
+          <span>Make: {boat.make}</span>
+          <span>Model: {boat.model}</span>
+          <span>Length: {boat.length}</span>
+        </div>
+      </div>
     </div>
   );
 }
