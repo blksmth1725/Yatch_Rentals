a73108962beb4ab86afb2f1f44473420fee20adb
Christian Sheen <csheen1725@gmail.com>
2021-06-13 10:56:44 -0400

    Recieved response still broken awaiting

--- yatch_rentals_frontend/pages/api/boats/index.js
+++ yatch_rentals_frontend/pages/api/boats/index.js
@@ -5,7 +5,7 @@ export default (req, res) => {
   if (req.method === "GET") {
     res.status(200).json(boats);
   } else {
-    res.setHeader("Allow", [GET]);
+    res.setHeader("Allow", ["GET"]);
     res.status(405).json({ message: `Method ${req.method} is not allowed` });
   }
 };
--- yatch_rentals_frontend/pages/boats/[slug].js
+++ yatch_rentals_frontend/pages/boats/[slug].js
@@ -5,14 +5,13 @@ export default function Boat({ boat }) {
   return (
     <Layout>
       <h1>This is supposed to be a single boat</h1>
-      <h1>{boat.name}</h1>
     </Layout>
   );
 }
 
 export async function getServerSideProps({ query: { slug } }) {
   const res = await fetch(`${API_URL}/api/boats/${slug}`);
-  const boats = JSON.stringify(await res.json());
+  const boats = await res.json();
   return {
     props: {
       boat: boats[0],
